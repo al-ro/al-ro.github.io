8dbe750ba4c5354b335649e2fbefae95622b5979
ar <na>
2017-09-18 23:12:39 +0100

    website

--- projects/maze/maze.js
+++ projects/maze/maze.js
@@ -5,7 +5,7 @@
 const mobile = ( navigator.userAgent.match(/Android/i)
     || navigator.userAgent.match(/webOS/i)
     || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPad/i)
+    //|| navigator.userAgent.match(/iPad/i)
     || navigator.userAgent.match(/iPod/i)
     || navigator.userAgent.match(/BlackBerry/i)
     || navigator.userAgent.match(/Windows Phone/i)
