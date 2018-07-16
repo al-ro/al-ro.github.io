93ad32e388284ae661f1bc40e38eef514dfdbfc8
al-ro <31544036+al-ro@users.noreply.github.com>
2018-07-16 20:19:03 +0100

    Update vortex.js
    Former-commit-id: 5359414d8915741a0beaf8f9f81651c6ea9f4330

--- projects/vortex/vortex.js
+++ projects/vortex/vortex.js
@@ -1,7 +1,3 @@
-//Based on:
-//https://aerotwist.com/tutorials/creating-particles-with-three-js/
-//http://petewerner.blogspot.co.uk/2015/02/intro-to-curl-noise.html
-
 var canvas = document.getElementById("canvas_1");
 var cont = document.getElementById("cc_1");
 
@@ -21,21 +17,21 @@ var height = 2000;
 var depth = 2000;
 var centre = [width/2,height/2, depth/2];
 
-var particleCount;
+var flakes = [];
+
+var flakeCount;
 
 if(mobile){
-  particleCount = 2000;
+  flakeCount = 2000;
 }else{
-  particleCount = 25000;
+  flakeCount = 2000;
 }
 
 var speed = 5;
 //Noise field zoom
-var step = 200;
+var step = 10;
 //Camera rotate
 var rotate = false;
-//Offset to counteract noise flattening when sampling on three planes
-var offset = 0.1;
 
 var ratio =  canvas.width / canvas.height;
 var w = cont.offsetWidth;
@@ -46,7 +42,7 @@ var scene = new THREE.Scene();
 
 var renderer = new THREE.WebGLRenderer({antialias: true, canvas: canvas});
 renderer.setSize(w,h,false);
-renderer.setClearColor(0xffffff);
+renderer.setClearColor(0x114457, 1);
 
 distance = 400;
 
@@ -55,10 +51,29 @@ var FOV = 2 * Math.atan( window.innerHeight / ( 2 * distance ) ) * 90 / Math.PI;
 var camera = new THREE.PerspectiveCamera(FOV, ratio, 1, 200000);
 
 camera.up.set(0,0,1);
-camera.position.set(0, -height, depth/2);
+camera.position.set(width/2, -height/2, -depth/2);
 camera.lookAt(new THREE.Vector3(centre[0], centre[1], centre[2]));
 
 scene.add(camera);
+//Lights
+var light_1 = new THREE.AmbientLight( 0xffffff, 1.8); 
+//scene.add(light_1);
+var light_2;
+light_2 = new THREE.DirectionalLight(0xff0000, 2.0);
+light_2.position.set(0, -1, 0);
+scene.add(light_2);
+var light_3;
+light_3 = new THREE.DirectionalLight(0x00ff00, 2.0);
+light_3.position.set(-1, 1, 0);
+scene.add(light_3);
+var light_4;
+light_4 = new THREE.DirectionalLight(0x3333ff, 2.0);
+light_4.position.set(0, 1, -1);
+scene.add(light_4);
+var light_5;
+light_5 = new THREE.DirectionalLight(0xffff00, 2.0);
+light_5.position.set(1, -1, 1);
+scene.add(light_5);
 
 //OrbitControls.js for camera manipulation
 controls = new THREE.OrbitControls( camera, renderer.domElement );
@@ -66,50 +81,86 @@ controls.maxDistance = 200000-5*width;
 controls.minDistance = 100;
 controls.autoRotate = rotate;
 
-//Particles
-var particles = new THREE.Geometry();
-//Initial ember colour
-var colour = 0xff0000;
-
-//Add texture to particles
+//Add texture to flakes
 //https://stackoverflow.com/questions/24087757/three-js-and-loading-a-cross-domain-image
 THREE.ImageUtils.crossOrigin = '';
 var texture = THREE.ImageUtils.loadTexture("http://res.cloudinary.com/al-ro/image/upload/c_scale,h_512/v1518264821/ember_ihk6rp.png");
 
-//Variable size for particle material
-var size = 50;
+var material = new THREE.MeshPhongMaterial( { color: 0x00AAFF, shininess: 30, specular: 0x111111  } );
 
-if(mobile){
-  size = 150;
+var loader = new THREE.STLLoader();
+loader.load( "https://github.com/al-ro/al-ro.github.io/blob/master/projects/vortex/ballerina_1.stl", function ( event ) {
+
+    var geometry = event.content;
+    geometry.computeTangents();
+    var mesh = new THREE.Mesh( geometry, material );
+
+    mesh.position.set( 0, -10, 0 );
+    mesh.rotation.set( - Math.PI / 2, 0, 0 );
+    mesh.scale.set( 15, 15, 15 );
+
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+
+    objects.push(mesh);
+    scene.add( mesh );
+
+} );
+
+
+var geom = new THREE.Geometry();
+
+//Brackets for purely aesthetic considerations
+{
+  geom.vertices.push(
+      new THREE.Vector3(   -0.5,  0.86, 0 ),
+      new THREE.Vector3(    0.5,  0.86, 0 ),
+      new THREE.Vector3(    0.93, 0.0,  0 ),
+      new THREE.Vector3(    0.5, -0.86, 0 ),
+      new THREE.Vector3(   -0.5, -0.86, 0 ),
+      new THREE.Vector3(   -0.93, 0.0, 0 )
+      );
 }
 
-var material = new THREE.PointsMaterial({
-  color: 0xff0000,
-    size: size,
-    transparent: true,
-    opacity: 1.0,
-    map: texture,
-    //Other particles show through transparent sections of texture
-    depthTest: false
-    //For glow effect
-    //,blending: THREE.AdditiveBlending
-});
+geom.faces.push( new THREE.Face3( 0, 1, 2 ) );
+geom.faces.push( new THREE.Face3( 0, 2, 3 ) );
+geom.faces.push( new THREE.Face3( 0, 3, 4 ) );
+geom.faces.push( new THREE.Face3( 0, 4, 5 ) );
 
-//Generate random particles
-for(i = 0; i < particleCount; i++){
-  var particle = {
-    x: width/2 - Math.random() * width,
-    y: height/2 - Math.random() * height,
-    z: depth/2 - Math.random() * depth,
-    vel_x: 0.5 - Math.random(),
-    vel_y: 0.5 - Math.random(),
-    vel_z: 0.5 - Math.random()
+geom.scale(7,7,7);
+
+var colour = 0xffffff;
+
+var material = new THREE.MeshPhongMaterial( {color: colour, specular: 0xff0000, shininess: 100, side: THREE.DoubleSide, shading: THREE.FlatShading} );
+
+
+//Generate random flakes
+for(i = 0; i < flakeCount; i++){
+
+  var g_ = new THREE.Mesh(geom, material);
+
+  var x = 0.5 - Math.random();
+  var y = 0.5 - Math.random();
+  var z = 0.5 - Math.random();
+
+  var flake = {
+    vel_x: x,
+    vel_y: y,
+    vel_z: z,
+    geo: g_
   };
-  particles.vertices.push(particle);
+
+  flake.geo.position.x = width/2 - Math.random() * width;
+  flake.geo.position.y = height/2 - Math.random() * height;
+  flake.geo.position.z = depth/2 - Math.random() * depth;
+
+  flakes.push(flake);
 }
 
-var particleSystem = new THREE.Points(particles, material);
-scene.add(particleSystem);
+for(i = 0; i < flakes.length; i++){
+  scene.add(flakes[i].geo);
+}
+
 
 //-----------GUI-----------//
 //dat.gui library controls
@@ -122,19 +173,19 @@ var reset_button = { reset:function(){
     console.log(err.message);
   }
 
-  for(i = 0; i < particleCount; i++){
+  for(i = 0; i < flakeCount; i++){
 
     var x = 0.5 - Math.random();
     var y = 0.5 - Math.random();
     var z = 0.5 - Math.random();
 
-    particles.vertices[i].vel_x = x;
-    particles.vertices[i].vel_y = y;
-    particles.vertices[i].vel_z = z;
+    flakes[i].vel_x = x;
+    flakes[i].vel_y = y;
+    flakes[i].vel_z = z;
 
-    particles.vertices[i].x = width/2 - Math.random() * width;
-    particles.vertices[i].y = height/2 - Math.random() * height;
-    particles.vertices[i].z = depth - Math.random() * depth;
+    flakes[i].geo.position.x = width/2 - Math.random() * width;
+    flakes[i].geo.position.y = height/2 - Math.random() * height;
+    flakes[i].geo.position.z = depth/2 - Math.random() * depth;
   }
 
 }};
@@ -145,21 +196,40 @@ var customContainer = document.getElementById('gui_container');
 customContainer.appendChild(gui.domElement);
 gui.add(this, 'speed').min(0).max(10).step(1).listen();
 gui.add(this, 'step').min(10).max(3000).step(10).listen();
-gui.add(this, 'size').min(1).max(300).step(1).listen().onChange(function(value) { setSize();} );;
 if(!mobile){
   gui.addColor(this, 'colour').listen().onChange(function(value) { setColour();} );
 }
-gui.add(this, 'offset').min(0.0).max(1.0).step(0.1).listen();
 gui.add(this, 'rotate').listen().onChange(function(value){ controls.autoRotate = rotate;});
 gui.add(reset_button, 'reset');
 gui.close();
 
-function setSize(){
-  material.size = size;
-}
-
 function setColour(){
   material.color.setHex(colour);
+}
+
+var A = {
+  x: 0,
+  y: 0,
+  z: 0
+};
+
+var B = {
+  x: 0,
+  y: 0,
+  z: 0
+}
+
+var n = {
+  x: 0,
+  y: 0,
+  z: 0
+};;
+
+//cross product
+function cross(A, B, n){
+  n.x = A.y * B.z - B.y * A.z;
+  n.y = A.z * B.x - B.z * A.x;
+  n.z = A.x * B.y - B.x * A.y;
 }
 
 //----------NOISE---------//
@@ -173,7 +243,7 @@ catch(err) {
   console.log(err.message);
 }
 
-//Find the curl of the noise field based on on the noise value at the location of a particle
+//Find the curl of the noise field based on on the noise value at the location of a flake
 function computeCurl(x, y, z){
   var eps = 0.0001;
 
@@ -217,32 +287,51 @@ function computeCurl(x, y, z){
 
 //----------MOVE----------//
 function move(){
-  for(i = 0; i < particleCount; i++){
+  for(i = 0; i < flakeCount; i++){
+      flakes[i].geo.rotation.x += flakes[i].vel_x/10+flakes[i].vel_z/100;
+      flakes[i].geo.rotation.y += flakes[i].vel_y/10+flakes[i].vel_z/100;
 
-    if(particles.vertices[i].z == 0){
-      particles.vertices[i].vel_x = 0.0; 
-      particles.vertices[i].vel_y = 0.0; 
-      particles.vertices[i].vel_z = 0.0; 
+
+      var curl = computeCurl(flakes[i].geo.position.x/step, flakes[i].geo.position.y/step, flakes[i].geo.position.z/step);
+      var mag_c = Math.sqrt(curl.x * curl.x + curl.y * curl.y + curl.z * curl.z);
+
+      A.x = flakes[i].geo.position.x;
+      A.y = flakes[i].geo.position.y;
+      A.z = flakes[i].geo.position.z;
+
+      var distanceFromCentre = Math.sqrt(A.x * A.x + A.y * A.y + A.z * A.z);  
+      if(distanceFromCentre > width/2){
+        flakes[i].geo.visible = false;
+      }else{
+        flakes[i].geo.visible = true;
+      }
+
+      B.z = 1;
+
+      cross(A, B, n);
+      var mag_n = Math.sqrt(n.x * n.x + n.y * n.y + n.z * n.z);
+      flakes[i].vel_x = n.x/mag_n;
+      flakes[i].vel_y = n.y/mag_n;
+
+      //Update flake velocity according to curl direction and speed
+      flakes[i].vel_x -= (speed/60)*(curl.x/mag_c);
+      flakes[i].vel_y -= (speed/60)*(curl.y/mag_c);
+
+      flakes[i].vel_z -= (speed/60);
+      flakes[i].vel_z = Math.max(flakes[i].vel_z, -speed);
+    
+
+    if(flakes[i].geo.position.z < -(depth/2) || Math.sqrt(A.x * A.x + A.y * A.y) > width){
+      flakes[i].geo.position.x = width/2 - Math.random() * width; 
+      flakes[i].geo.position.y = height/2 - Math.random() * height; 
+      flakes[i].geo.position.z = depth/2; 
+
     }else{
-      var curl = computeCurl(particles.vertices[i].x/step, particles.vertices[i].y/step, particles.vertices[i].z/step);
-      //Update particle velocity according to curl value and speed
-      particles.vertices[i].vel_x = curl.x/2;
-      particles.vertices[i].vel_y = curl.y/2;
-      particles.vertices[i].vel_z -= speed/60;
+      //Update flake position based on velocity
+      flakes[i].geo.position.x += flakes[i].vel_x ;
+      flakes[i].geo.position.y += flakes[i].vel_y ;
+      flakes[i].geo.position.z += flakes[i].vel_z ;
     }
-
-    if(particles.vertices[i].z < -(depth/2)){
-      particles.vertices[i].x = width/2 - Math.random() * width; 
-      particles.vertices[i].y = height/2 - Math.random() * height; 
-      particles.vertices[i].z = depth/2; 
-      particles.vertices[i].vel_z = 0; 
-    }else{
-      //Update particle position based on velocity
-      particles.vertices[i].x += particles.vertices[i].vel_x ;
-      particles.vertices[i].y += particles.vertices[i].vel_y ;
-      particles.vertices[i].z += particles.vertices[i].vel_z ;
-    }
-
   }
 }
 
@@ -253,8 +342,6 @@ function draw(){
     controls.update();
   }
   move();
-  //Redraws partciles
-  particles.verticesNeedUpdate = true;
   renderer.render(scene, camera);
   requestAnimationFrame(draw);
 }
