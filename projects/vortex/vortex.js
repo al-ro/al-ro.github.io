3953e503050eb9658c7a81b74cbd07ea84f402a8
al-ro <31544036+al-ro@users.noreply.github.com>
2018-07-16 20:20:02 +0100

    Update vortex.js
    Former-commit-id: b32f0e51249ba312ea1861a0b35a5ec0cacb9f96

--- projects/vortex/vortex.js
+++ projects/vortex/vortex.js
@@ -89,7 +89,7 @@ var texture = THREE.ImageUtils.loadTexture("http://res.cloudinary.com/al-ro/imag
 var material = new THREE.MeshPhongMaterial( { color: 0x00AAFF, shininess: 30, specular: 0x111111  } );
 
 var loader = new THREE.STLLoader();
-loader.load( "https://github.com/al-ro/al-ro.github.io/blob/master/projects/vortex/ballerina_1.stl", function ( event ) {
+loader.load( "./ballerina_1.stl", function ( event ) {
 
     var geometry = event.content;
     geometry.computeTangents();
