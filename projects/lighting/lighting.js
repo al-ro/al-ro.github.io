7f80b826a5555b4e669fe062cc1046cdcf98b001
ar <>
2019-05-01 17:28:53 +0200

    website

--- projects/lighting/lighting.js
+++ projects/lighting/lighting.js
@@ -303,7 +303,6 @@ function handle_load(gltf){
         mesh.children[0].material = new THREE.MeshBasicMaterial();
 	scene.add( mesh );
         mesh.position.y = 50;
-	var m = new THREE.mesh
 	console.log(gltf.scene.children[0]);
 
 
