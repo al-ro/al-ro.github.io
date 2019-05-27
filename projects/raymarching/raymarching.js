e84099d8de691086458fed399d17187ab401acb9
ar <>
2019-05-27 13:28:04 +0200

    website

--- projects/raymarching/raymarching.js
+++ projects/raymarching/raymarching.js
@@ -39,6 +39,7 @@ const stats = new Stats();
 stats.showPanel(0);
 stats.domElement.style.position = 'relative';
 stats.domElement.style.bottom = '48px';
+
 if(!mobile){
   document.getElementById('cc_1').appendChild(stats.domElement);
 }
@@ -132,8 +133,6 @@ vec3 rotate_xyz(vec3 p, float angle){
   position = rotate(position, vec4(0.0, 0.0, sa, ca));
   return position;
 }
-
-
 
 float sphereSDF(vec3 p, float radius) {
   return length(p) - radius;
@@ -320,7 +319,7 @@ float distanceToScene(vec3 cameraPos, vec3 rayDir, float start, float end) {
   return end;
 }
 
-//Ambinet occlusion reduces the ambinet light strength in areas 
+//Ambient occlusion reduces the ambient light strength in areas 
 //which are closely shielded by other objects
 //https://www.youtube.com/watch?v=22PZF7fWLqI
 float ambientOcclusion(vec3 position, vec3 normal){
@@ -543,54 +542,55 @@ function getUniformLocation(program, name) {
 
 //************** Create shaders **************
 
-//Create vertex and fragment shaders
-var vertexShader = compileShader(vertexSource, gl.VERTEX_SHADER);
-var fragmentShader = compileShader(fragmentSource, gl.FRAGMENT_SHADER);
+if(!mobile){
+  //Create vertex and fragment shaders
+  var vertexShader = compileShader(vertexSource, gl.VERTEX_SHADER);
+  var fragmentShader = compileShader(fragmentSource, gl.FRAGMENT_SHADER);
 
-//Create shader programs
-var program = gl.createProgram();
-gl.attachShader(program, vertexShader);
-gl.attachShader(program, fragmentShader);
-gl.linkProgram(program);
+  //Create shader programs
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
 
-gl.useProgram(program);
+  gl.useProgram(program);
 
-//Set up rectangle covering entire canvas 
-var vertexData = new Float32Array([
-    -1.0,  1.0, 	// top left
-    -1.0, -1.0, 	// bottom left
-    1.0,  1.0, 	// top right
-    1.0, -1.0, 	// bottom right
-]);
+  //Set up rectangle covering entire canvas 
+  var vertexData = new Float32Array([
+      -1.0,  1.0, 	// top left
+      -1.0, -1.0, 	// bottom left
+      1.0,  1.0, 	// top right
+      1.0, -1.0, 	// bottom right
+  ]);
 
-//Create vertex buffer
-var vertexDataBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
+  //Create vertex buffer
+  var vertexDataBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexDataBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
 
-// Layout of our data in the vertex buffer
-var positionHandle = getAttribLocation(program, 'position');
+  // Layout of our data in the vertex buffer
+  var positionHandle = getAttribLocation(program, 'position');
 
-gl.enableVertexAttribArray(positionHandle);
-gl.vertexAttribPointer(positionHandle,
+  gl.enableVertexAttribArray(positionHandle);
+  gl.vertexAttribPointer(positionHandle,
       2, 		// position is a vec2 (2 values per component)
       gl.FLOAT, 	// each component is a float
       false, 		// don't normalize values
       2 * 4, 		// two 4 byte float components per vertex (32 bit float is 4 bytes)
       0 		// how many bytes inside the buffer to start from
-    );
+      );
 
-//Set uniform handle
-var timeHandle = getUniformLocation(program, 'time');
-var widthHandle = getUniformLocation(program, 'width');
-var heightHandle = getUniformLocation(program, 'height');
-var sceneHandle = getUniformLocation(program, 'scene');
-var smoothnessHandle = getUniformLocation(program, 'k');
+  //Set uniform handle
+  var timeHandle = getUniformLocation(program, 'time');
+  var widthHandle = getUniformLocation(program, 'width');
+  var heightHandle = getUniformLocation(program, 'height');
+  var sceneHandle = getUniformLocation(program, 'scene');
+  var smoothnessHandle = getUniformLocation(program, 'k');
 
-gl.uniform1f(widthHandle, canvas.width);
-gl.uniform1f(heightHandle, canvas.height);
-gl.uniform1f(smoothnessHandle, smoothness);
-
+  gl.uniform1f(widthHandle, canvas.width);
+  gl.uniform1f(heightHandle, canvas.height);
+  gl.uniform1f(smoothnessHandle, smoothness);
+}
 //************** Draw **************
 function draw(){
   stats.begin();
@@ -603,9 +603,7 @@ function draw(){
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
   stats.end();
-  if(!mobile){
-    requestAnimationFrame(draw);
-  }
+  requestAnimationFrame(draw);
 }
 
 //Don't run if on mobile
