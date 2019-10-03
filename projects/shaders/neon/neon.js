2ad4f70a6b39beccc42227ca730d97526e082425
ar <>
2019-10-03 09:15:33 +0200

    website

--- projects/shaders/neon/neon.js
+++ projects/shaders/neon/neon.js
@@ -7,7 +7,7 @@ if(!gl){
 }
 
 //Time step
-var dt = 0.005;
+var dt = 0.012;
 //Time
 var time = 0.0;
 
@@ -29,46 +29,140 @@ vec2 resolution = vec2(width, height);
 
 uniform float time;
 
-//See https://www.shadertoy.com/view/3s3GDn for comments on the glow
-float getWaveGlow(vec2 pos, float radius, float intensity, float speed, float amplitude, float frequency, float shift){
+#define POINT_COUNT 8
 
-  float dist = abs((pos.y + amplitude * sin(shift + speed * time + pos.x * frequency)));
-  dist = 1.0/dist;
-  dist *= radius;
-  dist = pow(dist, intensity);
-  return dist;
+vec2 points[POINT_COUNT];
+const float speed = -0.5;
+const float len = 0.25;
+const float scale = 0.012;
+float intensity = 0.99;
+float radius = 0.02;
+
+//https://www.shadertoy.com/view/MlKcDD
+//Signed distance to a quadratic bezier
+float sdBezier(vec2 pos, vec2 A, vec2 B, vec2 C){    
+  vec2 a = B - A;
+  vec2 b = A - 2.0*B + C;
+  vec2 c = a * 2.0;
+  vec2 d = A - pos;
+
+  float kk = 1.0 / dot(b,b);
+  float kx = kk * dot(a,b);
+  float ky = kk * (2.0*dot(a,a)+dot(d,b)) / 3.0;
+  float kz = kk * dot(d,a);      
+
+  float res = 0.0;
+
+  float p = ky - kx*kx;
+  float p3 = p*p*p;
+  float q = kx*(2.0*kx*kx - 3.0*ky) + kz;
+  float h = q*q + 4.0*p3;
+
+  if(h >= 0.0){ 
+    h = sqrt(h);
+    vec2 x = (vec2(h, -h) - q) / 2.0;
+    vec2 uv = sign(x)*pow(abs(x), vec2(1.0/3.0));
+    float t = uv.x + uv.y - kx;
+    t = clamp( t, 0.0, 1.0 );
+
+    // 1 root
+    vec2 qos = d + (c + b*t)*t;
+    res = length(qos);
+  }else{
+    float z = sqrt(-p);
+    float v = acos( q/(p*z*2.0) ) / 3.0;
+    float m = cos(v);
+    float n = sin(v)*1.732050808;
+    vec3 t = vec3(m + m, -n - m, n - m) * z - kx;
+    t = clamp( t, 0.0, 1.0 );
+
+    // 3 roots
+    vec2 qos = d + (c + b*t.x)*t.x;
+    float dis = dot(qos,qos);
+
+    res = dis;
+
+    qos = d + (c + b*t.y)*t.y;
+    dis = dot(qos,qos);
+    res = min(res,dis);
+
+    qos = d + (c + b*t.z)*t.z;
+    dis = dot(qos,qos);
+    res = min(res,dis);
+
+    res = sqrt( res );
+  }
+
+  return res;
+}
+
+
+//http://mathworld.wolfram.com/HeartCurve.html
+vec2 getHeartPosition(float t){
+  return vec2(16.0 * sin(t) * sin(t) * sin(t),
+      -(13.0 * cos(t) - 5.0 * cos(2.0*t)
+	- 2.0 * cos(3.0*t) - cos(4.0*t)));
+}
+
+//https://www.shadertoy.com/view/3s3GDn
+float getGlow(float dist, float radius, float intensity){
+  return pow(radius/dist, intensity);
+}
+
+float getSegment(float t, vec2 pos, float offset){
+  for(int i = 0; i < POINT_COUNT; i++){
+    points[i] = getHeartPosition(offset + float(i)*len + fract(speed * t) * 6.28);
+  }
+
+  vec2 c = (points[0] + points[1]) / 2.0;
+  vec2 c_prev;
+  float dist = 10000.0;
+
+  for(int i = 0; i < POINT_COUNT-1; i++){
+    //https://tinyurl.com/y2htbwkm
+    c_prev = c;
+    c = (points[i] + points[i+1]) / 2.0;
+    dist = min(dist, sdBezier(pos, scale * c_prev, scale * points[i], scale * c));
+  }
+  return max(0.0, dist);
 }
 
 void main(){
-
   vec2 uv = gl_FragCoord.xy/resolution.xy;
   float widthHeightRatio = resolution.x/resolution.y;
   vec2 centre = vec2(0.5, 0.5);
   vec2 pos = centre - uv;
   pos.y /= widthHeightRatio;
+  //Shift upwards to centre heart
+  pos.y += 0.01;
 
-  float intensity = 1.0;
-  float radius = 0.03;
+  float t = time;
+
+  //Get first segment
+  float dist = getSegment(t, pos, 0.0);
+  float glow = getGlow(dist, radius, intensity);
 
   vec3 col = vec3(0.0);
-  float dist = 0.0;
 
-  //Use time varying colours from the basic template
-  //Add it to vec3(0.1) to always have a bright core
-  dist = getWaveGlow(pos, radius,intensity, 2.0, 0.018, 3.7, 0.0);
-  col += dist * (vec3(0.1) + 0.5 + 0.5*cos(3.14+time+vec3(0,2,4)));
+  //White core
+  col += 10.0*vec3(smoothstep(0.003, 0.001, dist));
+  //Pink glow
+  col += glow * vec3(0.94,0.14,0.4);
 
-  dist = getWaveGlow(pos, radius, intensity, 4.0, 0.018, 6.0, 2.0);
-  col += dist * (vec3(0.1) + 0.5 + 0.5*cos(1.57+time+vec3(0,2,4)));
+  //Get second segment
+  dist = getSegment(t, pos, 3.4);
+  glow = getGlow(dist, radius, intensity);
 
-  dist = getWaveGlow(pos, radius*0.5, intensity, -5.0, 0.018, 4.0, 1.0);
-  col += dist * (vec3(0.1) + 0.5 + 0.5*cos(time+vec3(0,2,4)));
+  //White core
+  col += 10.0*vec3(smoothstep(0.003, 0.001, dist));
+  //Blue glow
+  col += glow * vec3(0.2,0.6,1.0);
 
-  //Tone map function discussed in the comments of https://www.shadertoy.com/view/3s3GDn
+  //Tone mapping
   col = 1.0 - exp(-col);
 
-  // Output to screen
-  gl_FragColor = vec4(col, 1.0);
+  //Output to screen
+  gl_FragColor = vec4(col,1.0);
 }
 `;
 
