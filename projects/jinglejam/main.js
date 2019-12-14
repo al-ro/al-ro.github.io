55f16bc48990f66f7be83c956cefa809cbbe8ee5
al-ro <>
2019-12-15 00:11:29 +0100

    website

--- projects/jinglejam/main.js
+++ projects/jinglejam/main.js
@@ -378,7 +378,7 @@ for(var i = 0; i < lvl1.obstacleMap.length; i++){
       var glowMesh = new THREE.Mesh( cylinderGeometry, glowMaterial );
       break;
     case ObstacleType.TRAVELLER:
-      var glowMaterial = new THREE.MeshBasicMaterial( {color: 0xff3300} );
+      var glowMaterial = new THREE.MeshBasicMaterial( {color: 0xff0000} );
       var glowMesh = new THREE.Mesh( sphereGeometry, glowMaterial );
       break;
   }
