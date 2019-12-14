5f7930b7e31e26964ae369de1a9f37a40abc6c57
al-ro <>
2019-12-14 20:26:13 +0100

    website

--- projects/jinglejam/main.js
+++ projects/jinglejam/main.js
@@ -564,9 +564,7 @@ function checkCollision(){
     for(var i = 0; i < _obstacles.length; i++){
       direction.set(1,0);
       var rotation = obstacles[_obstacles[i]].rotation.y;
-      console.log(
-      direction.rotateAround(anchor, rotation);
-      )
+      console.log(direction.rotateAround(anchor, rotation));
     } 
   }
 }
