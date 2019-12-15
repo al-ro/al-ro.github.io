37e44002744353830b02e0245eefe88551641f9a
al-ro <>
2019-12-15 02:27:03 +0100

    website

--- projects/jinglejam/main.js
+++ projects/jinglejam/main.js
@@ -48,7 +48,7 @@ var songs = [];
 
   //Camera
   var camera = new THREE.PerspectiveCamera(FOV, window.innerWidth / window.innerHeight, 1, 20000);
-  var cameraStartPosition = new THREE.Vector3(-10, 30, -10);
+  var cameraStartPosition = new THREE.Vector3(-20, 30, -20);
   camera.position.copy(cameraStartPosition);
   camera.lookAt(0,0,0);
   scene.add(camera);
