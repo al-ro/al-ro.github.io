09edd6facc784201e6c9a1f8c6cc57da670f0cd6
al-ro <>
2019-12-15 01:33:28 +0100

    website

--- projects/jinglejam/main.js
+++ projects/jinglejam/main.js
@@ -355,7 +355,7 @@ class Obstacle {
 	this._meshGlow.rotateZ(this._movement.rZ);
 	break;
       case ObstacleType.TRAVELLER:
-	this._time = (this._time + speed * 0.25) % 6.283;
+	this._time = (this._time + speed * 0.2) % 6.283;
 	var x = this._originalPos.x + Math.sin(4*time) * 8;
 	var z = this._originalPos.z + Math.cos(3*time) * 8;
 	this._meshCore.position.set(x, 0, z);
