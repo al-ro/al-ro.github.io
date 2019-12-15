81eb303999c2a9050368b71b5d26359fb9e476b7
al-ro <>
2019-12-15 16:23:36 +0100

    website

--- projects/jinglejam/Audio.js
+++ projects/jinglejam/Audio.js
@@ -1,5 +1,13 @@
 var audio_ctx;
-var audio;
+var theme1;
+var theme2;
+var theme3;
+var menu;
+var laser;
+var end;
+var pickup;
+var delivery;
+
 var analyser;
 var frequencyCount;
 var frequencyData;
@@ -7,7 +15,14 @@ var timeData;
 
 function initAudio(){
   audio_ctx = new (window.AudioContext || window.webkitAudioContext)();
-  audio = new Audio('https://al-ro.github.io/projects/jinglejam/music/LVL1-cut.wav');
+  theme1 = new Audio('https://al-ro.github.io/projects/jinglejam/music/LVL1-cut.wav');
+  theme2 = new Audio('https://al-ro.github.io/projects/jinglejam/music/LVL2.wav');
+  theme3 = new Audio('https://al-ro.github.io/projects/jinglejam/music/LVL3.wav');
+  menu = new Audio('https://al-ro.github.io/projects/jinglejam/music/Main_Menu_C_WITH_MELODY.wav');
+  laser = new Audio('https://al-ro.github.io/projects/jinglejam/music/laser(hit).wav');
+  end = new Audio('https://al-ro.github.io/projects/jinglejam/music/CompletionMusic.wav');
+  pickup = new Audio('https://al-ro.github.io/projects/jinglejam/music/powerUp.wav');
+  delivery = new Audio('https://al-ro.github.io/projects/jinglejam/music/item_delivery_short.wav');
   audio.crossOrigin = "anonymous";
   var source = audio_ctx.createMediaElementSource(audio);
   analyser = audio_ctx.createAnalyser();
