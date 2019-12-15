fe145b89c078ff0e15a71a5ee680e4a64af51a33
al-ro <>
2019-12-15 01:34:34 +0100

    website

--- projects/jinglejam/main.js
+++ projects/jinglejam/main.js
@@ -637,8 +637,8 @@ function checkCollision(){
 	  // code block
 	  break;
 	case ObstacleType.TRAVELLER:
-	  var dx = player.position.x - obstacles[index].position.x;
-	  var dz = player.position.z - obstacles[index].position.z;
+	  var dx = player.position.x - obstacles[index].coreMesh.position.x;
+	  var dz = player.position.z - obstacles[index].coreMesh.position.z;
 	  if(Math.sqrt((dx*dx)+(dz*dz)) < sphereRadius){
 	    return true;
 	  }
