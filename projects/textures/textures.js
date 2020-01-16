880f298e6c5105b8e16260a669f8a8e101a8766e
al-ro <>
2020-01-16 14:23:03 +0100

    website
    Former-commit-id: 37cfa6a8485e112bf164d17a6f33ee346ea8a79a
    Former-commit-id: db56414e04fe1f755bf14d4abd13da1e1bb52aa9

--- projects/textures/textures.js
+++ projects/textures/textures.js
@@ -70,19 +70,164 @@ uniform float time;
 
 //WEBGL-NOISE FROM https://github.com/stegu/webgl-noise
 
-//Description : Array and textureless GLSL 2D simplex noise function. Author : Ian McEwan, Ashima Arts. Maintainer : stegu Lastmod : 20110822 (ijm) License : Copyright (C) 2011 Ashima Arts. All rights reserved. Distributed under the MIT License. See LICENSE file. https://github.com/ashima/webgl-noise https://github.com/stegu/webgl-noise
+//Description : Array and textureless GLSL 2D/3D simplex noise function. Author : Ian McEwan, Ashima Arts. Maintainer : stegu Lastmod : 20110822 (ijm) License : Copyright (C) 2011 Ashima Arts. All rights reserved. Distributed under the MIT License. See LICENSE file. https://github.com/ashima/webgl-noise https://github.com/stegu/webgl-noise
 
-vec3 mod289(vec3 x) {return x - floor(x * (1.0 / 289.0)) * 289.0;} vec2 mod289(vec2 x) {return x - floor(x * (1.0 / 289.0)) * 289.0;} vec3 permute(vec3 x) {return mod289(((x*34.0)+1.0)*x);} float snoise(vec2 v){const vec4 C = vec4(0.211324865405187, 0.366025403784439, -0.577350269189626, 0.024390243902439); vec2 i  = floor(v + dot(v, C.yy) ); vec2 x0 = v -   i + dot(i, C.xx); vec2 i1; i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0); vec4 x12 = x0.xyxy + C.xxzz; x12.xy -= i1; i = mod289(i); vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 )) + i.x + vec3(0.0, i1.x, 1.0 )); vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0); m = m*m ; m = m*m ; vec3 x = 2.0 * fract(p * C.www) - 1.0; vec3 h = abs(x) - 0.5; vec3 ox = floor(x + 0.5); vec3 a0 = x - ox; m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h ); vec3 g; g.x  = a0.x  * x0.x  + h.x  * x0.y; g.yz = a0.yz * x12.xz + h.yz * x12.yw; return 130.0 * dot(m, g);}
+//2D
+//vec3 mod289(vec3 x){return x-floor(x*(1.0/289.0))*289.0;}vec2 mod289(vec2 x){return x-floor(x*(1.0/289.0))*289.0;}vec3 permute(vec3 x){return mod289(((x*34.0)+1.0)*x);}float snoise(vec2 v){const vec4 C=vec4(0.211324865405187,0.366025403784439,-0.577350269189626,0.024390243902439);vec2 i=floor(v+dot(v,C.yy));vec2 x0=v-i+dot(i,C.xx);vec2 i1;i1=(x0.x>x0.y)?vec2(1.0,0.0):vec2(0.0,1.0);vec4 x12=x0.xyxy+C.xxzz;x12.xy-=i1;i=mod289(i);vec3 p=permute(permute(i.y+vec3(0.0,i1.y,1.0))+i.x+vec3(0.0,i1.x,1.0));vec3 m=max(0.5-vec3(dot(x0,x0),dot(x12.xy,x12.xy),dot(x12.zw,x12.zw)),0.0);m=m*m;m=m*m;vec3 x=2.0*fract(p*C.www)-1.0;vec3 h=abs(x)-0.5;vec3 ox=floor(x+0.5);vec3 a0=x-ox;m*=1.79284291400159-0.85373472095314*(a0*a0+h*h);vec3 g;g.x=a0.x*x0.x+h.x*x0.y;g.yz=a0.yz*x12.xz+h.yz*x12.yw;return 130.0*dot(m,g);}
+
+//3D
+vec3 mod289(vec3 x){return x-floor(x*(1.0/289.0))*289.0;}vec4 mod289(vec4 x){return x-floor(x*(1.0/289.0))*289.0;}vec4 permute(vec4 x){return mod289(((x*34.0)+1.0)*x);}vec4 taylorInvSqrt(vec4 r){return 1.79284291400159-0.85373472095314*r;}float snoise(vec3 v, float frequency){const vec2 C=vec2(1.0/6.0,1.0/3.0);const vec4 D=vec4(0.0,0.5,1.0,2.0);vec3 i=floor(v+dot(v,C.yyy));vec3 x0=v-i+dot(i,C.xxx);vec3 g=step(x0.yzx,x0.xyz);vec3 l=1.0-g;vec3 i1=min(g.xyz,l.zxy);vec3 i2=max(g.xyz,l.zxy);vec3 x1=x0-i1+C.xxx;vec3 x2=x0-i2+C.yyy;vec3 x3=x0-D.yyy;i=mod289(i);vec4 p=permute(permute(permute(i.z+vec4(0.0,i1.z,i2.z,1.0))+i.y+vec4(0.0,i1.y,i2.y,1.0))+i.x+vec4(0.0,i1.x,i2.x,1.0)); p = mod(p, frequency);float n_=0.142857142857;vec3 ns=n_*D.wyz-D.xzx;vec4 j=p-49.0*floor(p*ns.z*ns.z);vec4 x_=floor(j*ns.z);vec4 y_=floor(j-7.0*x_);vec4 x=x_*ns.x+ns.yyyy;vec4 y=y_*ns.x+ns.yyyy;vec4 h=1.0-abs(x)-abs(y);vec4 b0=vec4(x.xy,y.xy);vec4 b1=vec4(x.zw,y.zw);vec4 s0=floor(b0)*2.0+1.0;vec4 s1=floor(b1)*2.0+1.0;vec4 sh=-step(h,vec4(0.0));vec4 a0=b0.xzyw+s0.xzyw*sh.xxyy;vec4 a1=b1.xzyw+s1.xzyw*sh.zzww;vec3 p0=vec3(a0.xy,h.x);vec3 p1=vec3(a0.zw,h.y);vec3 p2=vec3(a1.xy,h.z);vec3 p3=vec3(a1.zw,h.w);vec4 norm=taylorInvSqrt(vec4(dot(p0,p0),dot(p1,p1),dot(p2,p2),dot(p3,p3)));p0*=norm.x;p1*=norm.y;p2*=norm.z;p3*=norm.w;vec4 m=max(0.6-vec4(dot(x0,x0),dot(x1,x1),dot(x2,x2),dot(x3,x3)),0.0);m=m*m;return 42.0*dot(m*m,vec4(dot(p0,x0),dot(p1,x1),dot(p2,x2),dot(p3,x3)));}
+
+vec4 fade(vec4 t){
+  return (t * t * t) * (t * (t * vec4(6) - vec4(15)) + vec4(10));
+}
+
+//From https://github.com/g-truc/glm/blob/master/glm/gtc/noise.inl
+// Classic Perlin noise, periodic version
+float glmPerlin(vec4 Position, vec4 rep){
+  vec4 Pi0 = mod(floor(Position), rep); // Integer part modulo rep
+  vec4 Pi1 = mod(Pi0 + float(1), rep); // Integer part + 1 mod rep
+  vec4 Pf0 = fract(Position); // Fractional part for interpolation
+  vec4 Pf1 = Pf0 - float(1); // Fractional part - 1.0
+  vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+  vec4 iy = vec4(Pi0.y, Pi0.y, Pi1.y, Pi1.y);
+  vec4 iz0 = vec4(Pi0.z);
+  vec4 iz1 = vec4(Pi1.z);
+  vec4 iw0 = vec4(Pi0.w);
+  vec4 iw1 = vec4(Pi1.w);
+
+  vec4 ixy = permute(permute(ix) + iy);
+  vec4 ixy0 = permute(ixy + iz0);
+  vec4 ixy1 = permute(ixy + iz1);
+  vec4 ixy00 = permute(ixy0 + iw0);
+  vec4 ixy01 = permute(ixy0 + iw1);
+  vec4 ixy10 = permute(ixy1 + iw0);
+  vec4 ixy11 = permute(ixy1 + iw1);
+
+  vec4 gx00 = ixy00 / float(7);
+  vec4 gy00 = floor(gx00) / float(7);
+  vec4 gz00 = floor(gy00) / float(6);
+  gx00 = fract(gx00) - float(0.5);
+  gy00 = fract(gy00) - float(0.5);
+  gz00 = fract(gz00) - float(0.5);
+  vec4 gw00 = vec4(0.75) - abs(gx00) - abs(gy00) - abs(gz00);
+  vec4 sw00 = step(gw00, vec4(0));
+  gx00 -= sw00 * (step(float(0), gx00) - float(0.5));
+  gy00 -= sw00 * (step(float(0), gy00) - float(0.5));
+
+  vec4 gx01 = ixy01 / float(7);
+  vec4 gy01 = floor(gx01) / float(7);
+  vec4 gz01 = floor(gy01) / float(6);
+  gx01 = fract(gx01) - float(0.5);
+  gy01 = fract(gy01) - float(0.5);
+  gz01 = fract(gz01) - float(0.5);
+  vec4 gw01 = vec4(0.75) - abs(gx01) - abs(gy01) - abs(gz01);
+  vec4 sw01 = step(gw01, vec4(0.0));
+  gx01 -= sw01 * (step(float(0), gx01) - float(0.5));
+  gy01 -= sw01 * (step(float(0), gy01) - float(0.5));
+
+  vec4 gx10 = ixy10 / float(7);
+  vec4 gy10 = floor(gx10) / float(7);
+  vec4 gz10 = floor(gy10) / float(6);
+  gx10 = fract(gx10) - float(0.5);
+  gy10 = fract(gy10) - float(0.5);
+  gz10 = fract(gz10) - float(0.5);
+  vec4 gw10 = vec4(0.75) - abs(gx10) - abs(gy10) - abs(gz10);
+  vec4 sw10 = step(gw10, vec4(0.0));
+  gx10 -= sw10 * (step(float(0), gx10) - float(0.5));
+  gy10 -= sw10 * (step(float(0), gy10) - float(0.5));
+
+  vec4 gx11 = ixy11 / float(7);
+  vec4 gy11 = floor(gx11) / float(7);
+  vec4 gz11 = floor(gy11) / float(6);
+  gx11 = fract(gx11) - float(0.5);
+  gy11 = fract(gy11) - float(0.5);
+  gz11 = fract(gz11) - float(0.5);
+  vec4 gw11 = vec4(0.75) - abs(gx11) - abs(gy11) - abs(gz11);
+  vec4 sw11 = step(gw11, vec4(float(0)));
+  gx11 -= sw11 * (step(float(0), gx11) - float(0.5));
+  gy11 -= sw11 * (step(float(0), gy11) - float(0.5));
+
+  vec4 g0000 = vec4(gx00.x, gy00.x, gz00.x, gw00.x);
+  vec4 g1000 = vec4(gx00.y, gy00.y, gz00.y, gw00.y);
+  vec4 g0100 = vec4(gx00.z, gy00.z, gz00.z, gw00.z);
+  vec4 g1100 = vec4(gx00.w, gy00.w, gz00.w, gw00.w);
+  vec4 g0010 = vec4(gx10.x, gy10.x, gz10.x, gw10.x);
+  vec4 g1010 = vec4(gx10.y, gy10.y, gz10.y, gw10.y);
+  vec4 g0110 = vec4(gx10.z, gy10.z, gz10.z, gw10.z);
+  vec4 g1110 = vec4(gx10.w, gy10.w, gz10.w, gw10.w);
+  vec4 g0001 = vec4(gx01.x, gy01.x, gz01.x, gw01.x);
+  vec4 g1001 = vec4(gx01.y, gy01.y, gz01.y, gw01.y);
+  vec4 g0101 = vec4(gx01.z, gy01.z, gz01.z, gw01.z);
+  vec4 g1101 = vec4(gx01.w, gy01.w, gz01.w, gw01.w);
+  vec4 g0011 = vec4(gx11.x, gy11.x, gz11.x, gw11.x);
+  vec4 g1011 = vec4(gx11.y, gy11.y, gz11.y, gw11.y);
+  vec4 g0111 = vec4(gx11.z, gy11.z, gz11.z, gw11.z);
+  vec4 g1111 = vec4(gx11.w, gy11.w, gz11.w, gw11.w);
+
+  vec4 norm00 = taylorInvSqrt(vec4(dot(g0000, g0000), dot(g0100, g0100), dot(g1000, g1000), dot(g1100, g1100)));
+  g0000 *= norm00.x;
+  g0100 *= norm00.y;
+  g1000 *= norm00.z;
+  g1100 *= norm00.w;
+
+  vec4 norm01 = taylorInvSqrt(vec4(dot(g0001, g0001), dot(g0101, g0101), dot(g1001, g1001), dot(g1101, g1101)));
+  g0001 *= norm01.x;
+  g0101 *= norm01.y;
+  g1001 *= norm01.z;
+  g1101 *= norm01.w;
+
+  vec4 norm10 = taylorInvSqrt(vec4(dot(g0010, g0010), dot(g0110, g0110), dot(g1010, g1010), dot(g1110, g1110)));
+  g0010 *= norm10.x;
+  g0110 *= norm10.y;
+  g1010 *= norm10.z;
+  g1110 *= norm10.w;
+
+  vec4 norm11 = taylorInvSqrt(vec4(dot(g0011, g0011), dot(g0111, g0111), dot(g1011, g1011), dot(g1111, g1111)));
+  g0011 *= norm11.x;
+  g0111 *= norm11.y;
+  g1011 *= norm11.z;
+  g1111 *= norm11.w;
+
+  float n0000 = dot(g0000, Pf0);
+  float n1000 = dot(g1000, vec4(Pf1.x, Pf0.y, Pf0.z, Pf0.w));
+  float n0100 = dot(g0100, vec4(Pf0.x, Pf1.y, Pf0.z, Pf0.w));
+  float n1100 = dot(g1100, vec4(Pf1.x, Pf1.y, Pf0.z, Pf0.w));
+  float n0010 = dot(g0010, vec4(Pf0.x, Pf0.y, Pf1.z, Pf0.w));
+  float n1010 = dot(g1010, vec4(Pf1.x, Pf0.y, Pf1.z, Pf0.w));
+  float n0110 = dot(g0110, vec4(Pf0.x, Pf1.y, Pf1.z, Pf0.w));
+  float n1110 = dot(g1110, vec4(Pf1.x, Pf1.y, Pf1.z, Pf0.w));
+  float n0001 = dot(g0001, vec4(Pf0.x, Pf0.y, Pf0.z, Pf1.w));
+  float n1001 = dot(g1001, vec4(Pf1.x, Pf0.y, Pf0.z, Pf1.w));
+  float n0101 = dot(g0101, vec4(Pf0.x, Pf1.y, Pf0.z, Pf1.w));
+  float n1101 = dot(g1101, vec4(Pf1.x, Pf1.y, Pf0.z, Pf1.w));
+  float n0011 = dot(g0011, vec4(Pf0.x, Pf0.y, Pf1.z, Pf1.w));
+  float n1011 = dot(g1011, vec4(Pf1.x, Pf0.y, Pf1.z, Pf1.w));
+  float n0111 = dot(g0111, vec4(Pf0.x, Pf1.y, Pf1.z, Pf1.w));
+  float n1111 = dot(g1111, Pf1);
+
+  vec4 fade_xyzw = fade(Pf0);
+  vec4 n_0w = mix(vec4(n0000, n1000, n0100, n1100), vec4(n0001, n1001, n0101, n1101), fade_xyzw.w);
+  vec4 n_1w = mix(vec4(n0010, n1010, n0110, n1110), vec4(n0011, n1011, n0111, n1111), fade_xyzw.w);
+  vec4 n_zw = mix(n_0w, n_1w, fade_xyzw.z);
+  vec2 n_yzw = mix(vec2(n_zw.x, n_zw.y), vec2(n_zw.z, n_zw.w), fade_xyzw.y);
+  float n_xyzw = mix(n_yzw.x, n_yzw.y, fade_xyzw.x);
+  return float(2.2) * n_xyzw;
+}
+
 //END NOISE
 
 #define TILES 1.0 // Use 1.0 for normal tiling across whole texture.
 #define NUM_CELLS 4.0
 
 
-float remap(float value, float old_low, float old_high, float new_low, float new_high){
-  float ret_val = new_low + (value - old_low) * (new_high - new_low) / (old_high - old_low);
-  return ret_val; 
+float remap(float x, float low1, float high1, float low2, float high2){
+  return low2 + (x - low1) * (high2 - low2) / (high1 - low1);
 }
+float saturate(float x){
+  return clamp(x, 0.0, 1.0);
+}
+
 
 
 //----------------------------------------------------------------------------------------
@@ -156,6 +301,8 @@ float worley(vec3 pos, float numCells){
 }
 
 //----------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------
 float Noise(vec2 pos, float scale )
 {
   vec2 f;
@@ -168,48 +315,14 @@ float Noise(vec2 pos, float scale )
   f = f*f*(3.0-2.0*f);	// Cosine interpolation approximation
 
   float res = mix(mix(Hash(p, scale), 
-		      Hash(p + vec2(1.0, 0.0), scale), f.x),
-		  mix(Hash(p + vec2(0.0, 1.0), scale),
-		      Hash(p + vec2(1.0, 1.0), scale), f.x), 
-	      f.y);
+	Hash(p + vec2(1.0, 0.0), scale), f.x),
+      mix(Hash(p + vec2(0.0, 1.0), scale),
+	Hash(p + vec2(1.0, 1.0), scale), f.x), 
+      f.y);
   return res;
 }
 
 //----------------------------------------------------------------------------------------
-float fbm(vec2 pos){
-  float f = 0.0;
-  // Change starting scale to any integer value...
-  float scale = 10.;
-  vec2 p  = mod(pos, scale);
-  float amp   = 0.5;
-
-  for (int i = 0; i < 5; i++)
-  {
-    f += Noise(p, scale) * amp;
-    amp *= 0.5;
-    // Scale must be multiplied by an integer value...
-    scale *= 2.0;
-  }
-  // Clamp it just in case....
-  return min(f, 1.0);
-}
-
-float fbm(vec2 pos, float numCells){
-  float f = 0.0;
-  // Change starting scale to any integer value...
-  float scale = 1.0;
-  vec2 p  = mod(pos, scale);
-  float amp  = 0.5;
-
-  for (int i = 0; i < 3; i++){
-    f += worley(p, numCells * scale) * amp;
-    amp *= 0.5;
-    // Scale must be multiplied by an integer value...
-    scale *= 2.0;
-  }
-  // Clamp it just in case....
-  return min(f, 1.0);
-}
 
 //Return the 3D coordinate corresponding to the 2D atlas uv coordinate.
 //If invalid, return -1.0
@@ -221,12 +334,54 @@ vec3 get3Dfrom2D(vec2 uv, float tileRows){
   return vec3(fract(uv), z);
 }
 
+float getPerlinNoise(vec3 pos, float frequency){
+  //Noise frequency factor between octave, forced to 2
+  const float octaveFrequencyFactor = 2.0;
+
+  // Compute the sum for each octave
+  float sum = 0.0;
+  float weightSum = 0.0;
+  float weight = 0.5;
+  for(int oct = 0; oct < 3; oct++){
+
+    vec3 p = pos * frequency;
+    //Hillaire says that the 3D version of tileable perlin noise in GLM is broken
+    //Use 4D noise with w component set to 0 as in example code
+    float val = 0.5 + 0.5 * glmPerlin(vec4(p, 0.0), vec4(frequency));
+
+    sum += val * weight;
+    weightSum += weight;
+
+    //Original had multiplication with weight but most sources say to reduce amplitude by half
+    weight *= 0.5;
+    frequency *= octaveFrequencyFactor;
+  }
+
+  float noise = (sum / weightSum);
+  noise = saturate(noise);
+  return noise;
+}
+
 //type 0 == perlin-worley
 //type 1 == worley
 float getTextureForPoint(vec3 p, int type){
   float res;
   if(type == 0){
-    //Perlin-Worley (Really, Simplex-Worley)
+    //Perlin-Worley
+    //const float frequencyMul[6] = float[6]( 2.0,8.0,14.0,20.0,26.0,32.0 );
+    //const int octaveCount = 3;
+    const float frequency = 8.0;
+    float perlinNoise = getPerlinNoise(p, frequency);
+    res = perlinNoise;
+
+    //Special weights from example code
+    float worley0 = worley(p, NUM_CELLS*2.0);
+    float worley1 = worley(p, NUM_CELLS*8.0);
+    float worley2 = worley(p, NUM_CELLS*14.0);
+
+    float worleyFBM = worley0 * 0.625 + worley1 * 0.25 + worley2 * 0.125;
+    res = remap(perlinNoise, 0.0, 1.0, worleyFBM, 1.0);
+
 
   }else{
     //Worley
@@ -304,46 +459,46 @@ void main() {
   vec3 p = p_;
   p.z /= (tileRows*tileRows);
 
-  vec3 col = vec3(0);
+  vec4 col = vec4(0,0,0,1);
   //Get Perlin-Worley noise for level l
   col.r = getTextureForPoint(p, 0);
   //Get Worley noise for level l
-  col.r = getTextureForPoint(p, 1);
+  //col.b = getTextureForPoint(p, 1);
 
   p_ = mod(p_ + 1.0, tileRows * tileRows);
   p = p_;
   p.z /= (tileRows*tileRows); 
   //Get Perlin-Worley noise for level l+1
-  col.r = getTextureForPoint(p, 0);
+  col.g = getTextureForPoint(p, 0);
   //Get Worley noise for level l+1
-  col.g = getTextureForPoint(p, 1);
+  //col.a = getTextureForPoint(p, 1);
 
   //Show erroneous values as red
-  if((col.x > 1.0) || (col.x < 0.0)){ col = vec3(1,0,0); }
+  if((col.x > 1.0) || (col.x < 0.0)){ col = vec4(1,0,0,1); }
 
   //Boundary cells
   if(padCell){
-    //col = vec3(0,0,1);
+    //col = vec4(0,0,1,1);
   }
   //iterate layers
   if(floor(p_.z) == floor(mod(time, tileRows * tileRows))){
     if(padCell){
-      //col = vec3(1,1,0);
+      //col = vec4(1,1,0,1);
     }else{
-      //col = vec3(0,1,0);
+      //col = vec4(0,1,0,1);
     }
   }
   if(startPadX){
-    //col /= vec3(1,1,0);
+    //col /= vec4(1,1,0,1);
   }
   if(startPadY){
-    //col /= vec3(1,1,0);
+    //col /= vec4(1,1,0,1);
   }
   //Unused cells
   if(gl_FragCoord.x > tileRows * tileSize || gl_FragCoord.y > tileRows * tileSize){
-    col = vec3(0,0,0);
+    col = vec4(0,0,0,1);
   }
-  gl_FragColor = vec4(col, 1.0);
+  gl_FragColor = col;
 }
 `;
 
