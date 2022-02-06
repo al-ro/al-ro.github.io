9d46c31c76b82eda9af64b5feaee9f036d390fc1
al-ro <>
2022-02-06 13:46:06 +0100

    website
    Former-commit-id: 0610e56579869967d2b4c5280967846bd09c4b88
    Former-commit-id: ed8eb3f84493ac41e938ca8aabf0431d08f6f592

--- projects/renderer/screenspace.js
+++ projects/renderer/screenspace.js
@@ -1,11 +1,11 @@
 import {Geometry} from "./geometry.js";
 import {gl} from "./canvas.js";
 
-var vertexData = new Float32Array([
-    -1.0,  1.0, 0.0,	// top left
-    -1.0, -1.0, 0.0,	// bottom left
-     1.0,  1.0, 0.0,	// top right
-     1.0, -1.0, 0.0	// bottom right
+const vertexData = new Float32Array([
+    -1.0,  1.0, 0.0,  // top left
+    -1.0, -1.0, 0.0,  // bottom left
+     1.0,  1.0, 0.0,  // top right
+     1.0, -1.0, 0.0   // bottom right
 ]);
 
 function getScreenspaceQuad(){
