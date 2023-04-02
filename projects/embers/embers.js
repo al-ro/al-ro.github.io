e3c76a15eebb8b0b0fad9f756e83149163e6c726
al-ro <>
2023-04-02 16:18:56 +0200

    website

--- projects/embers/embers.js
+++ projects/embers/embers.js
@@ -26,13 +26,11 @@ if(mobile){
   particleCount = 25000;
 }
 
-var speed = 5;
+var speed = 10;
 //Noise field zoom
-var step = 2000;
+var step = 1500;
 //Camera rotate
 var rotate = true;
-//Offset to counteract noise flattening when sampling on three planes
-var offset = 0.0;
 
 var w = canvas.clientWidth;
 var h = canvas.clientHeight;
@@ -78,6 +76,9 @@ controls.autoRotate = rotate;
 //Particles
 var particles = [];
 var velocities = [];
+var times = [];
+var lifetimes = [];
+var positions = [];
 var geometry = new THREE.BufferGeometry();
 //Initial ember colour
 var colour = 0xff6800;
@@ -110,15 +111,16 @@ var material = new THREE.PointsMaterial({
 //Generate random particles
 for(i = 0; i < particleCount; i++){
 
-    let x = width/2 - Math.random() * width;
-    let y = height/2 - Math.random() * height;
-    let z = depth/2 - Math.random() * depth;
-    let vel_x = 0.5 - Math.random();
-    let vel_y = 0.5 - Math.random();
-    let vel_z = 0.5 - Math.random();
+  var x = width/2 - Math.random() * width;
+  var y = height/2 - Math.random() * height;
+  var z = depth/2 - Math.random() * depth;
   
   particles.push(x,y,z);
-	velocities.push(vel_x,vel_y,vel_z);
+  positions.push(x,y,z);
+  velocities.push( 0.5 - Math.random(), 0.5 - Math.random(), 0.5 - Math.random());
+
+  times.push(0);
+  lifetimes.push(3.0 * Math.random());
 }
 
 geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( particles, 3 ) );
@@ -133,12 +135,11 @@ var reset_button = { reset:function(){
   //Use noise.js library to generate a grid of 3D simplex noise values
   try {
     noise.seed(Math.random());
-  }
-  catch(err) {
+  }catch(err) {
     console.log(err.message);
   }
 
-  for(i = 0; i < particleCount * 3.0; i+=3){
+  for(i = 0; i < particleCount * 3.0; i += 3){
 
     var x = 0.5 - Math.random();
     var y = 0.5 - Math.random();
@@ -159,15 +160,14 @@ var reset_button = { reset:function(){
 var gui = new dat.GUI({ autoPlace: false });
 var customContainer = document.getElementById('gui_container');
 customContainer.appendChild(gui.domElement);
-gui.add(this, 'speed').min(0).max(10).step(1).listen();
+gui.add(this, 'speed').min(0).max(100).step(1).listen();
 gui.add(this, 'step').min(10).max(3000).step(10).listen();
-gui.add(this, 'size').min(1).max(300).step(1).listen().onChange(function(value) { setSize();} );;
+gui.add(this, 'size').min(1).max(300).step(1).listen().onChange(() => setSize() );
+gui.add(this, 'rotate').listen().onChange(() => {controls.autoRotate = rotate});
+gui.add(reset_button, 'reset');
 if(!mobile){
   gui.addColor(this, 'colour').listen().onChange(function(value) { setColour();} );
 }
-gui.add(this, 'offset').min(0.0).max(1.0).step(0.1).listen();
-gui.add(this, 'rotate').listen().onChange(function(value){ controls.autoRotate = rotate;});
-gui.add(reset_button, 'reset');
 gui.close();
 
 function setSize(){
@@ -188,12 +188,24 @@ catch(err) {
   console.log(err.message);
 }
 
+function cross(a, b){
+  return [ a[1] * b[2] - a[2] * b[1],
+	   a[2] * b[0] - a[0] * b[2],
+	   a[0] * b[1] - a[1] * b[0]
+	 ]; 
+}
+
+function normalize(v){
+  var length = Math.sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+  return [v[0]/length, v[1]/length, v[2]/length];
+}
+
 function fbm(x, y, z){
-  let n = 0;
-  let l = 1.0;
-  let totalWeight = 0.0;
-  let amplitude = 1.0;
-  for(let i = 0; i < 1; i++){
+  var n = 0;
+  var l = 1.0;
+  var totalWeight = 0.0;
+  var amplitude = 1.0;
+  for(var i = 0; i < 1; i++){
     n += amplitude * noise.simplex3(x*l, y*l, z*l);
     totalWeight += amplitude;
     amplitude *= 0.5;
@@ -206,10 +218,7 @@ function fbm(x, y, z){
 
 //Find the curl of the noise field based on on the noise value at the location of a particle
 function computeCurl(x, y, z){
-  var eps = 0.0001;
-
-  x += 1000.0*offset;
-  y -= 1000.0*offset;
+  var eps = 1e-4;
 
   var curl = new THREE.Vector3();
 
@@ -224,6 +233,7 @@ function computeCurl(x, y, z){
   var b = (n1 - n2)/(2 * eps);
   curl.x = a - b;
 
+  x += 100.0;
   //Find rate of change in ZX plane
   n1 = fbm(x, y, z + eps); 
   n2 = fbm(x, y, z - eps); 
@@ -235,13 +245,14 @@ function computeCurl(x, y, z){
   b = (n1 - n2)/(2 * eps);
   curl.y = a - b;
 
+  y += 2000.0;
   //Find rate of change in XY plane
-  n1 = noise.simplex3(x + eps, y, z); 
-  n2 = noise.simplex3(x - eps, y, z); 
+  n1 = fbm(x + eps, y, z); 
+  n2 = fbm(x - eps, y, z); 
   //Average to find approximate derivative
   a = (n1 - n2)/(2 * eps);
-  n1 = noise.simplex3(x, y + eps, z); 
-  n2 = noise.simplex3(x, y - eps, z); 
+  n1 = fbm(x, y + eps, z); 
+  n2 = fbm(x, y - eps, z); 
   //Average to find approximate derivative
   b = (n1 - n2)/(2 * eps);
   curl.z = a - b;
@@ -250,8 +261,8 @@ function computeCurl(x, y, z){
 }
 
 //----------MOVE----------//
-function move(){
-  for(i = 0; i < particleCount * 3.0; i+=3){
+function move(dT){
+  for(i = 0; i < particleCount * 3.0; i += 3){
 
     //Find curl value at partile location
     var curl = computeCurl(particles[i]/step, particles[i+1]/step, particles[i+2]/step);
@@ -266,13 +277,16 @@ function move(){
     particles[i+1] += velocities[i+1];
     particles[i+2] += velocities[i+2];
 
-    //Boudnary conditions
-    //If a particle gets too far away from (0,0,0), reset it to a random location
-    var dist = Math.sqrt(particles[i] * particles[i] + particles[i+1] * particles[i+1] + particles[i+2] * particles[i+2]);
-    if(dist > 5.0*width){
-      particles[i] = width/2 - Math.random() * width;
-      particles[i+1] = height/2 - Math.random() * height;
-      particles[i+2] = depth/2 - Math.random() * depth; 
+    times[Math.floor(i/3)] += dT;
+
+    //Boundary conditions
+    //If a particle gets too far away from the origin or has existed for a set time, reset it to a random location
+    var dist = particles[i] * particles[i] + particles[i+1] * particles[i+1] + particles[i+2] * particles[i+2];
+    if(dist > (25.0 * width * width) || times[Math.floor(i/3)] > lifetimes[Math.floor(i/3)]){
+      particles[i] = positions[i] + times[Math.floor(i/3)];
+      particles[i+1] = positions[i+1] + times[Math.floor(i/3)];
+      particles[i+2] = positions[i+2] +times[Math.floor(i/3)];
+      times[Math.floor(i/3)] = 0.0;
     }
   }
 	
@@ -280,12 +294,24 @@ function move(){
 	geometry.getAttribute('position').needsUpdate = true;
 	
 }
+
+var lastFrame = Date.now();
+var thisFrame;
+var time = 0;
+
 //----------DRAW----------//
 function draw(){
 	if(rotate){
     controls.update();
   }
-  move();
+
+  thisFrame = Date.now();
+
+  var dT = (thisFrame - lastFrame)/1000;
+  time += dT;
+  lastFrame = thisFrame;
+
+  move(dT);
   renderer.render(scene, camera);
   requestAnimationFrame(draw);
 }
