2929b6b4f989682095e566f7e05451e4fd68a084
al-ro <a.a>
2025-02-12 02:02:08 +0200

    Pointer event handling

--- projects/renderer/controls.js
+++ projects/renderer/controls.js
@@ -1,26 +1,36 @@
 import { canvas, gl, canvasMultiplier } from "./canvas.js"
 export class Controls {
 
-  isMouseDown = false;
   lastPosition = [0, 0];
 
-  mouseDelta = [0, 0];
+  pointerDelta = [0, 0];
 
   maxDistance = 1000.0;
 
+  resolutionMultiplier = canvasMultiplier;
+
   camera;
+
+  eventCache = [];
+  prevDiff = -1;
 
   constructor(camera) {
     this.camera = camera;
 
     //https://stackoverflow.com/questions/49091584/javascript-es6-addeventlistener-inside-class
-    canvas.addEventListener('mousedown', this.mouseDown.bind(this));
-    canvas.addEventListener('mouseup', this.mouseUp.bind(this));
-    canvas.addEventListener('mousemove', this.mouseMove.bind(this));
+    canvas.addEventListener('pointerdown', this.pointerDown.bind(this));
+    canvas.addEventListener('pointerup', this.pointerUp.bind(this));
+    canvas.addEventListener('pointerleave', this.pointerUp.bind(this));
+    canvas.addEventListener('pointercancel', this.pointerUp.bind(this));
+    canvas.addEventListener('pointerout', this.pointerUp.bind(this));
+    canvas.addEventListener('pointermove', this.pointerMove.bind(this));
     canvas.addEventListener('wheel', this.onScroll.bind(this));
     window.addEventListener('resize', this.onWindowResize.bind(this), false);
-    document.addEventListener('keydown', this.keyDown.bind(this));
-    document.addEventListener('keyup', this.keyUp.bind(this));
+  }
+
+  setMultiplier(multiplier) {
+    this.resolutionMultiplier = multiplier;
+    this.onWindowResize();
   }
 
   getPosition(canvas, evt) {
@@ -28,25 +38,59 @@ export class Controls {
     return [evt.clientX - rect.left, evt.clientY - rect.top];
   }
 
-  mouseDown(event) {
-    this.isMouseDown = true;
+  pointerDown(event) {
+    this.eventCache.push(event);
     this.lastPosition = this.getPosition(canvas, event);
   }
 
-  mouseUp(event) {
-    this.isMouseDown = false;
-    this.mouseDelta = [0, 0];
+  removeEvent(event) {
+    const index = this.eventCache.findIndex(
+      (cachedEv) => cachedEv.pointerId === event.pointerId,
+    );
+    this.eventCache.splice(index, 1);
   }
 
-  mouseMove(event) {
-    if (this.isMouseDown) {
-      let position = this.getPosition(canvas, event);
-      this.mouseDelta[0] = this.lastPosition[0] - position[0];
-      this.mouseDelta[1] = this.lastPosition[1] - position[1];
+  pointerUp(event) {
+    this.pointerDelta = [0, 0];
+    this.removeEvent(event);
 
-      this.camera.updatePosition(this.mouseDelta);
+    if (this.eventCache.length < 2) {
+      this.prevDiff = -1;
+    }
+  }
 
-      this.lastPosition = position;
+  pointerMove(event) {
+    const index = this.eventCache.findIndex(
+      (cachedEv) => cachedEv.pointerId === event.pointerId,
+    );
+    this.eventCache[index] = event;
+
+    if (this.eventCache.length === 2) {
+
+      const curDiff = Math.hypot(
+        this.eventCache[0].clientX - this.eventCache[1].clientX,
+        this.eventCache[0].clientY - this.eventCache[1].clientY);
+
+      if (this.prevDiff > 0) {
+        let dist = this.camera.distance;
+        dist += (this.prevDiff - curDiff) * 0.01;
+        dist = Math.min(Math.max(0.0001, dist), this.maxDistance);
+        this.camera.updateDistance(dist);
+      }
+
+      this.prevDiff = curDiff;
+
+    } else {
+
+      if (event.buttons > 0) {
+        let position = this.getPosition(canvas, event);
+        this.pointerDelta[0] = this.lastPosition[0] - position[0];
+        this.pointerDelta[1] = this.lastPosition[1] - position[1];
+
+        this.camera.updatePosition(this.pointerDelta);
+
+        this.lastPosition = position;
+      }
     }
   }
 
@@ -56,76 +100,6 @@ export class Controls {
     dist += event.deltaY * 0.001;
     dist = Math.min(Math.max(0.0001, dist), this.maxDistance);
     this.camera.updateDistance(dist);
-  }
-
-
-  //************** User movement **************
-  forward = false;
-  backward = false;
-  left = false;
-  right = false;
-
-  keyDown(e) {
-    if (e.keyCode == 38 || e.keyCode == 40) {
-      e.preventDefault();
-    }
-    if (e.keyCode == 87 || e.keyCode == 38) {
-      this.forward = true;
-    }
-    if (e.keyCode == 83 || e.keyCode == 40) {
-      this.backward = true;
-    }
-    if (e.keyCode == 65 || e.keyCode == 37) {
-      this.left = true;
-    }
-    if (e.keyCode == 68 || e.keyCode == 39) {
-      this.right = true;
-    }
-  };
-
-  keyUp(e) {
-    if (e.keyCode == 87 || e.keyCode == 38) {
-      this.forward = false;
-    }
-    if (e.keyCode == 83 || e.keyCode == 40) {
-      this.backward = false;
-    }
-    if (e.keyCode == 65 || e.keyCode == 37) {
-      this.left = false;
-    }
-    if (e.keyCode == 68 || e.keyCode == 39) {
-      this.right = false;
-    }
-  };
-
-  move(dT) {
-
-    let speed = 10.0;
-    let viewDirection = this.camera.viewDirection;
-    let rightDirection = this.camera.rightDirection;
-    let position = this.camera.position;
-
-    if (this.forward) {
-      position[0] += dT * speed * viewDirection[0];
-      position[1] += dT * speed * viewDirection[1];
-      position[2] += dT * speed * viewDirection[2];
-    }
-    if (this.backward) {
-      position[0] -= dT * speed * viewDirection[0];
-      position[1] -= dT * speed * viewDirection[1];
-      position[2] -= dT * speed * viewDirection[2];
-    }
-    if (this.left) {
-      position[0] += dT * speed * rightDirection[0];
-      position[1] += dT * speed * rightDirection[1];
-      position[2] += dT * speed * rightDirection[2];
-    }
-    if (this.right) {
-      position[0] -= dT * speed * rightDirection[0];
-      position[1] -= dT * speed * rightDirection[1];
-      position[2] -= dT * speed * rightDirection[2];
-    }
-
   }
 
   setPageLayout(w, h) {
@@ -145,8 +119,8 @@ export class Controls {
     if (!isInFullscreen()) {
       h = w / 1.6;
     }
-    canvas.width = w * canvasMultiplier;
-    canvas.height = h * canvasMultiplier;
+    canvas.width = w * this.resolutionMultiplier;
+    canvas.height = h * this.resolutionMultiplier;
 
     this.setPageLayout(w, h);
 
